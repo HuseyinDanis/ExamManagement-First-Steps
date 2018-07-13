cdf7c45ab51172c626a88213c7d3cc240ef51e70
Hüseyin Danismanlioglu <husyndanis@gmail.com>
2018-07-13 12:14:07 +0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text =="admin" && textBox2.Text =="1234")
+            {
+                Form3 yeni = new Form3();
+                yeni.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Giriş");
+            }
+        }
+    }
+}
